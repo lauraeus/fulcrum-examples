3d4ca64af6b3329af161ee21dca27cd7ddf146e4
Lars Lindberg <lars.lindberg@xlent.se>
2017-07-01 18:51:24 +0200

    Bll tests pass.

---
+++ SupplierCompany.SystemFacade/test/Bll/GeocodingTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SupplierCompany.SystemFacade.Dal.RestClients.GoogleGeocode.Clients;
+using SM = SupplierCompany.SystemFacade.Fulcrum.Contract;
+using DM = SupplierCompany.SystemFacade.Dal.RestClients.GoogleGeocode.Models;
+
+namespace SupplierCompany.SystemFacade.Bll.Tests
+{
+    [TestClass]
+    public class GeocodingTest
+    {
+        private Mock<IGeocodeClient> _geocodeClientMock;
+        private IGeocodingFunctionality _geocodeFunctionality;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+            _geocodeClientMock = new Mock<IGeocodeClient>();
+            _geocodeFunctionality = new GeocodingFunctionality(_geocodeClientMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Success()
+        {
+            _geocodeClientMock.Setup(mock => mock.GeocodeAsync(It.IsAny<DM.Address>())).Returns(Task.FromResult(CreateResponse()));
+            var address = new SM.Address
+            {
+                Row1 = "Regeringsgatan 67",
+                PostCode = "11156",
+                Country = "Sweden"
+            };
+            var location = await _geocodeFunctionality.GeocodeAsync(address);
+            Assert.IsNotNull(location);
+            Assert.IsNotNull(location.Latitude);
+            Assert.IsNotNull(location.Longitude);
+
+        }
+
+        private DM.GeocodingResponse CreateResponse()
+        {
+            return new DM.GeocodingResponse
+            {
+                status = "OK",
+                results = new []
+                {
+                    new DM.Result
+                    {
+                        geometry = new DM.Geometry
+                        {
+                            location = new DM.Location
+                            {
+                                lng = "-122.0842499",
+                                lat = "37.4224764"
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
--- SupplierCompany.SystemFacade/test/Dal.RestClients.Test/GeocodingTest.cs
+++ SupplierCompany.SystemFacade/test/Dal.RestClients.Test/GeocodingTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SupplierCompany.SystemFacade.Dal.RestClients.GoogleGeocode.Clients;
 using SupplierCompany.SystemFacade.Dal.RestClients.GoogleGeocode.Models;
 
@@ -16,15 +18,21 @@ namespace SystemFacade.Dal.RestClients.Test
         }
 
         [TestMethod]
-        public void Successful()
+        public async Task Successful()
         {
             var address = new Address
             {
                 Row1 = "Regeringsgatan 67",
-                PostCode = "11156"
+                PostCode = "11156",
+                Country = "Sweden"
             };
-            var location = _client.GeocodeAsync(address);
-            Assert.IsNotNull(location);
+            var response = await _client.GeocodeAsync(address);
+            Assert.IsNotNull(response?.results);
+            Assert.AreEqual("OK", response.status);
+            var result = response?.results?.FirstOrDefault();
+            var location = result?.geometry?.location;
+            Assert.IsNotNull(location.lat);
+            Assert.IsNotNull(location.lng);
         }
     }
 }
