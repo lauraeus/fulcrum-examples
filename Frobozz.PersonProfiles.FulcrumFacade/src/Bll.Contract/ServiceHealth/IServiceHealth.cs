8b5c184e7f03e6ddacd551c9f176908259664b11
Lars Lindberg <lars.lindberg@xlent.se>
2017-07-08 08:18:37 +0200

    Changed from 4.1.1.1

---
+++ Frobozz.PersonProfiles.FulcrumFacade/src/Bll.Contract/ServiceHealth/IServiceHealth.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Xlent.Lever.Libraries2.Standard.Health.Model;
+
+namespace Xlent.Lever.KeyTranslator.Facade.WebApi.Controllers
+{
+    public interface IServiceHealth
+    {
+        Task<HealthResponse> ServiceHealthAsync();
+    }
+}
