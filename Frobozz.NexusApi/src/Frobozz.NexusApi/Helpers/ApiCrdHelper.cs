1eacee112d38bc2da5b7dcf8095d1e1df1b9c553
Lars Lindberg <lars.lindberg@xlent.se>
2018-04-01 15:03:46 +0200

    Added ConsentsController.

---
+++ Frobozz.NexusApi/src/Frobozz.NexusApi/Helpers/ApiCrdHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xlent.Lever.Libraries2.Core.Assert;
+using Xlent.Lever.Libraries2.Core.Storage.Model;
+
+namespace Frobozz.NexusApi.Helpers
+{
+    public class ApiCrdHelper<TModel> : ApiReaderHelper<TModel>, ICrd<TModel, string>
+    where TModel : IValidatable
+    {
+        private readonly ICrd<TModel, string> _storage;
+
+        public ApiCrdHelper(ICrd<TModel, string> storage)
+        :base(storage)
+        {
+            _storage = storage;
+        }
+
+        /// <inheritdoc />
+        public async Task<string> CreateAsync(TModel item)
+        {
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            return await _storage.CreateAsync(item);
+        }
+
+        /// <inheritdoc />
+        public async Task<TModel> CreateAndReturnAsync(TModel item)
+        {
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            return await _storage.CreateAndReturnAsync(item);
+        }
+
+        /// <inheritdoc />
+        public async Task CreateWithSpecifiedIdAsync(string id, TModel item)
+        {
+            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            await _storage.CreateWithSpecifiedIdAsync(id, item);
+        }
+
+        /// <inheritdoc />
+        public async Task<TModel> CreateWithSpecifiedIdAndReturnAsync(string id, TModel item)
+        {
+            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            return await _storage.CreateWithSpecifiedIdAndReturnAsync(id, item);
+        }
+  
+
+        /// <inheritdoc />
+        public async Task DeleteAsync(string id)
+        {
+            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
+            await _storage.DeleteAsync(id);
+        }
+
+        /// <inheritdoc />
+        public async Task DeleteAllAsync()
+        {
+            await _storage.DeleteAllAsync();
+        }
+    }
+}
--- Frobozz.NexusApi/src/Frobozz.NexusApi/Helpers/ApiCrudHelper.cs
+++ Frobozz.NexusApi/src/Frobozz.NexusApi/Helpers/ApiCrudHelper.cs
@@ -1,92 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xlent.Lever.Libraries2.Core.Assert;
 using Xlent.Lever.Libraries2.Core.Storage.Model;
 
 namespace Frobozz.NexusApi.Helpers
 {
-    public class ApiCrudHelper<TModel> : ICrud<TModel, string>
+    public class ApiCrudHelper<TModel> : ApiCrdHelper<TModel>, ICrud<TModel, string>
+    where TModel : IValidatable
     {
         private readonly ICrud<TModel, string> _storage;
 
         public ApiCrudHelper(ICrud<TModel, string> storage)
+        :base(storage)
         {
             _storage = storage;
         }
-
-        /// <inheritdoc />
-        public async Task<string> CreateAsync(TModel item)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <inheritdoc />
-        public async Task<TModel> CreateAndReturnAsync(TModel item)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <inheritdoc />
-        public async Task CreateWithSpecifiedIdAsync(string id, TModel item)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <inheritdoc />
-        public async Task<TModel> CreateWithSpecifiedIdAndReturnAsync(string id, TModel item)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <inheritdoc />
-        public async Task<TModel> ReadAsync(string id)
-        {
-            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
-            return await _storage.ReadAsync(id);
-        }
-
-        /// <inheritdoc />
-        public async Task<PageEnvelope<TModel>> ReadAllWithPagingAsync(int offset, int? limit = null)
-        {
-            ServiceContract.RequireGreaterThanOrEqualTo(0, offset, nameof(offset));
-            if (limit != null)
-            {
-                ServiceContract.RequireGreaterThan(0, limit.Value, nameof(limit));
-            }
-
-            return await _storage.ReadAllWithPagingAsync(offset, limit);
-        }
-
-        /// <inheritdoc />
-        public async Task<IEnumerable<TModel>> ReadAllAsync(int limit = int.MaxValue)
-        {
-            ServiceContract.RequireGreaterThan(0, limit, nameof(limit));
-            return await _storage.ReadAllAsync(limit);
-        }
-
-        /// <inheritdoc />
-        public async Task DeleteAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <inheritdoc />
-        public async Task DeleteAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
         /// <inheritdoc />
         public async Task UpdateAsync(string id, TModel item)
         {
-            throw new NotImplementedException();
+            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            await _storage.UpdateAsync(id, item);
         }
 
         /// <inheritdoc />
         public async Task<TModel> UpdateAndReturnAsync(string id, TModel item)
         {
-            throw new NotImplementedException();
+            ServiceContract.RequireNotNullOrWhitespace(id, nameof(id));
+            ServiceContract.RequireNotNull(item, nameof(item));
+            ServiceContract.RequireValidated(item, nameof(item));
+            return await _storage.UpdateAndReturnAsync(id, item);
         }
     }
 }
