46474b0a59e60bad97d78b8aefe6b52aa0e21c8f
Lars Lindberg <lars.lindberg@xlent.se>
2018-12-28 11:53:44 +0100

    Added a .NET Core WebApp

---
+++ Frobozz.GdprConsent.NexusFacade/src/GdprConsent.NexusAdapter.WebApp/Controllers/PersonsController.cs
@@ -0,0 +1,24 @@
+﻿using Frobozz.Contracts.AspNet.GdprCapability.Controllers;
+using Frobozz.Contracts.GdprCapability.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frobozz.GdprConsent.NexusAdapter.WebApp.Controllers
+{
+    /// <summary>
+    /// ApiController for Product that does inputcontrol. Logic is separated into another layer. 
+    /// </summary>
+    [Route("api")]
+    public class PersonsController : PersonServiceController
+    {
+        private readonly IGdprCapability _logic;
+
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        public PersonsController(IGdprCapability logic)
+        :base(logic)
+        {
+            _logic = logic;
+        }
+    }
+}
---
+++ Frobozz.GdprConsent.NexusFacade/src/GdprConsent.NexusAdapter.WebApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Frobozz.GdprConsent.NexusAdapter.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
